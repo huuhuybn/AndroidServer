668b2f0ef34235e9fe192b6bfa0b562259157f2b
HuyNguyen <huuhuybn@gmail.com>
2022-07-23 16:40:07 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/getUsers', function (req, res) {
+    fs.readFile('data/data.txt', function (err, data) {
+
+        if (err != null) {
+            res.send(err.message);
+        } else
+            res.send(data.toString());
+    })
+})
+
+module.exports = router;
